cca8e56570742a01bc0f3b582ed8013cd7a1c960
scom-01 <78716085+scom-01@users.noreply.github.com>
2022-02-11 01:39:06 +0900

    0211_Crosshair_1

---
+++ 02.Scripts/CrossHair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHair : MonoBehaviour
+{
+    Image CrossImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CrossImg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CrossImg.rectTransform.position = new Vector3(CrossImg.canvas.renderingDisplaySize.x / 2, CrossImg.canvas.renderingDisplaySize.y / 2, 0);
+        CrossImg.rectTransform.sizeDelta = new Vector2(GlobalValue.CH_Width, GlobalValue.CH_Height);
+        CrossImg.fillAmount = GlobalValue.CH_Height_Fill;
+        //Debug.Log(CrossImg.rectTransform.sizeDelta.x);
+        //CrossImg.rectTransform.pivot.y
+    }
+}
--- 02.Scripts/GlobalValue/GlobalValue.cs
+++ 02.Scripts/GlobalValue/GlobalValue.cs
@@ -7,6 +7,12 @@ public class GlobalValue : MonoBehaviour
     public static float MouseXSpeed = 1.0f;
     public static float MouseYSpeed = 1.0f;
     public static float deltaTime = 0.02f;
+
+    #region CrossHair
+    public static float CH_Width = 10;
+    public static float CH_Height = 50;
+    public static float CH_Height_Fill = 1;
+    #endregion
 }
 
 public enum CharState
